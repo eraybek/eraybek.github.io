ee103171147029679355a6d7bb498b0814841b3a
Eray Aybek <aybek17@itu.edu.tr>
2023-01-27 20:10:31 +0300

    visitor counter updated

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var counterContainerEl = document.querySelector(".website-counter");
+var resetButtonEl = document.querySelector("#reset");
+var visitCountEl = localStorage.getItem("page_view");
+
+if (visitCountEl) {
+    visitCountEl= Number(visitCountEl) + 1;
+    localStorage.setItem("page_view", visitCountEl);
+}else {
+    visitCountEl = 1;
+    localStorage.setItem("page_view", 1);
+}
+
+counterContainerEl.innerHTML += visitCountEl;
